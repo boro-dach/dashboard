2dd728ceea9ccdefd7bf1b9a2de50137169043a5
boro-dach <140444699+boro-dach@users.noreply.github.com>
2024-07-17 18:58:45 +0200

    chart test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button"
+import General from "@/components/Cards/General";
 
 export default function Home() {
   return (
     <div>
-      Homepage
+      <General />
     </div>
   )
 }
